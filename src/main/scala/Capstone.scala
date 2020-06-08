8d5e2859641c52ae7f67e42007e714da61b3cf90
Scott Armstrong <imabearnose@gmail.com>
2020-06-08 11:16:09 -0700

    completed model training

--- src/main/scala/Capstone.scala
+++ src/main/scala/Capstone.scala
@@ -26,10 +26,10 @@ import org.apache.spark.ml.linalg.{Vector, Vectors};
 import org.apache.spark.ml.linalg.SQLDataTypes.VectorType;
 
 import org.apache.spark.ml.classification.MultilayerPerceptronClassifier;
-import org.apache.spark.ml.classification.LinearSVC;
+import org.apache.spark.ml.classification.{LinearSVC, RandomForestClassifier};
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator;
 
-//import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler, OneHotEncoderEstimator};
+import org.apache.spark.ml.feature.VectorAssembler;//{StringIndexer, VectorAssembler, OneHotEncoderEstimator};
 //import org.apache.spark.ml.Pipeline;
 // import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 
@@ -45,107 +45,164 @@ object Capstone {
       .builder
       .appName("Capstone")
       .config("spark.master", "local[*]")
-      //.config("spark.master", "spark://ee:7077")
+//      .config("spark.master", "spark://ee:7077")
 //      .config("spark.master", "spark://192.168.1.2:7077")
       .getOrCreate();
 
-	import spark.implicits._; // some helpful conversions
+//	import spark.implicits._; // some helpful conversions
+//
+//	var new_labels = spark
+//		.read
+//		.format("csv")
+//		.option("inferSchema", true)
+//		.option("header", true)
+//		.load("data/train-calibrated-shuffled.csv");
+//
+//	var labels = Seq(
+//		(0, 0),
+//	   	(1, 0),
+//	   	(2, 0),
+//	   	(3, 0),
+//	   	(4, 0),
+//	   	(5, 0),
+//	   	(6, 0),
+//	   	(7, 0),
+//	   	(8, 0),
+//	   	(9, 0),
+//	   	(10, 1),
+//	   	(11, 1),
+//	   	(12, 1),
+//	   	(13, 1),
+//	   	(14, 1),
+//	   	(15, 1),
+//	   	(16, 1),
+//	   	(17, 1),
+//	   	(18, 1),
+//	   	(19, 1)
+//	).toDF("id", "thelabel");
+//
+//	// convert Array[Byte] to Vector for use as features
+//	val binaryToShortUDF = udf(
+//		(image: Array[Byte]) => {	
+//			Vectors.dense(
+//				image.map(
+//					// convert unsigned byte to signed byte and minmax scale
+//					x => (x & 0xFF) / 255.0
+//				)
+//			)
+//		},
+//		VectorType // custom spark sql type created by spark.ml.linalg.SQLTypes
+//	);
+//	spark.udf.register("binaryToShort", binaryToShortUDF);
+//
+//
+//	val imageDir = "data/test/"; 
+//	var images = ImageSchema.readImages(
+//		path = imageDir,
+//		sparkSession = spark,
+//		recursive = false,
+//		numPartitions = 1,
+//		dropImageFailures = false,
+//		sampleRatio = 1.0,
+//		seed = 2
+//	).map(
+//		image => {
+//			var row = image.getAs[Row](0);
+//			ImageSchema.getData(row)
+//		}
+//	)
+//
+//
+//	var asdf = images.select(
+//			binaryToShortUDF(col("value")) as "features"
+//		).withColumn("id", monotonically_increasing_id)
+//		.join(labels, "id")
+//		.withColumnRenamed("thelabel", "label");
+//
+////	var asdf = spark.createDataFrame(
+////		images.select(
+////			binaryToShortUDF(col("value")) as "features"
+////		).rdd,
+////		StructType(Array(StructField("features", VectorType, false))) // the dataframe is an Array[Vector] at heart
+////	)
+////		.withColumn("id", monotonically_increasing_id)
+////		.join(labels, "id")
+////		.withColumnRenamed("thelabel", "label");
+////	var the_df = images//asdf
+////		.withColumn("id", monotonically_increasing_id)
+////		.join(labels, "id")
+////		.withColumnRenamed("thelabel", "label");
+//
+//
+
+	var trainImages = spark.read
+		.format("csv")
+		.option("inferSchema", true)
+		.option("header", false)
+		.load("data/processed_csv/train_images.csv")
+		.withColumn("label", col("_c4096").cast(IntegerType));
+
+	var valImages = spark.read
+		.format("csv")
+		.option("inferSchema", true)
+		.option("header", false)
+		.load("data/processed_csv/val_images.csv")
+		.withColumn("label", col("_c4096").cast(IntegerType));
+
+	var testImages = spark.read
+		.format("csv")
+		.option("inferSchema", true)
+		.option("header", false)
+		.load("data/processed_csv/test_images.csv")
+		.withColumn("label", col("_c4096").cast(IntegerType));
 
 
-	val imageDir = "data/test/"; 
-	var images = ImageSchema.readImages(
-		path = imageDir,
-		sparkSession = spark,
-		recursive = false,
-		numPartitions = 1,
-		dropImageFailures = false,
-		sampleRatio = 1.0,
-		seed = 2
-	)
+	var assembler = new VectorAssembler()
+		.setInputCols(Array.range(0, 4096).map(x => s"_c$x"))
+		.setOutputCol("features");
 
-	var image_data = images
-		.map(
-			image => {
-				var row = image.getAs[Row](0);
-				ImageSchema.getData(row);	
-			}
-		)
-
-
-	val binaryToShort = udf(
-		(image: Array[Byte]) => {	
-			Vectors.dense(
-				image.map(
-					x => (x & 0xFF) / 255.0
-				)
-			)
-		},
-		VectorType
-	);
-	spark.udf.register("binaryToShort", binaryToShort);
-
-	var asdf = spark.createDataFrame(
-		image_data.select(
-			binaryToShort(col("value")) as "features"
-		).rdd,
-		StructType(Array(StructField("features", VectorType, false)))
-	);
-
-
-	var labels = Seq(
-		(0, 0),
-		(1, 0),
-		(2, 0),
-		(3, 0),
-		(4, 0),
-		(5, 0),
-		(6, 0),
-		(7, 0),
-		(8, 0),
-		(9, 0),
-		(10, 1),
-		(11, 1),
-		(12, 1),
-		(13, 1),
-		(14, 1),
-		(15, 1),
-		(16, 1),
-		(17, 1),
-		(18, 1),
-		(19, 1)
-	).toDF("id", "thelabel");
-
-	var the_df = asdf
-		.withColumn("id", monotonically_increasing_id)
-		.join(labels, "id")
-		.withColumnRenamed("thelabel", "label");
-
+	trainImages = assembler transform trainImages;
+	valImages = assembler transform valImages;
+	testImages = assembler transform testImages;
 
   	// Initializing model
-  	var Array(train, test) = the_df randomSplit (Array(0.8, 0.2), 2);
   	var evaluator = new MulticlassClassificationEvaluator()
 		.setMetricName("accuracy");
 
-	asdf = spark.emptyDataFrame;
-//	the_df = spark.emptyDataFrame;
-	image_data = spark.emptyDataset[Array[Byte]];
-	images = spark.emptyDataFrame;
+//    val layers = Array[Int](4096, 1024, 16, 2); // input features, hidden layers, output classes
+//    var nn = new MultilayerPerceptronClassifier()
+//    	.setLayers(layers)
+//    	.setBlockSize(128)
+//    	.setSeed(2L)
+//    	.setMaxIter(10);
 
-    val layers = Array[Int](4096, 1024, 16, 2); // input features, hidden layers, output classes
-    var nn = new MultilayerPerceptronClassifier()
-    	.setLayers(layers)
-    	.setBlockSize(128)
-    	.setSeed(2L)
-    	.setMaxIter(10);
-
+//	var rf = new RandomForestClassifier()
+//		.setFeatureSubsetStrategy("sqrt")
+//		.setMaxDepth(7)
+//		.setNumTrees(128)
+//		.setSeed(2L);
+	
+	var svc = new LinearSVC()
+		.setMaxIter(25)
+		.setRegParam(0.01);
 
   	// Modeling
-  	var nnModel = nn fit the_df;
-  	var predictions = nnModel transform the_df;
+//  	var nnModel = nn fit trainImages;
+//  	var predictions = nnModel transform valImages;
+
+//	var rfModel = rf fit trainImages;
+//	var predictions = rfModel transform valImages;
+
+	var svcModel = svc fit trainImages;
+	var predictions = svcModel transform valImages;
+
   	println(s"Evaluation ${evaluator evaluate predictions}");
+
 
     // ---
 	println("\n-----\nDone!\n-----\n");
+
+//	trainImages show;
 
 //	asdf printSchema;
 //	asdf show;
